1f6a4754e74303bc2c903899ee65beaf95f39726
Andrew White <white.d.andrew@gmail.com>
2021-08-29 23:00:21 -0400

    Simplified

--- src/app.js
+++ src/app.js
@@ -10,9 +10,7 @@ footer_hider = (event) => {
     var a = event.currentSlide.attributes.getNamedItem('hide-footer');
 
     if (a) {
-        if (a.nodeValue === 'yes') {
-            document.querySelector('.footer').style.visibility = 'hidden';
-        }
+        document.querySelector('.footer').style.visibility = 'hidden';
     } else {
         document.querySelector('.footer').style.visibility = 'visible';
     }
