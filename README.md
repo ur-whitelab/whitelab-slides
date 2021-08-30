49d21f565f1d99fe0618136b34272bff34a35f4b
Andrew White <white.d.andrew@gmail.com>
2021-08-29 22:37:27 -0400

    Added parcel and new version

---
+++ README.md
@@ -0,0 +1,14 @@
+# whitelab slides
+
+Make sure you have node installed. Then
+
+```sh
+npm i
+npm start
+```
+
+To bundle with it is as small as possible:
+
+```sh
+npm build
+```
---
+++ src/app.js
@@ -0,0 +1,28 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Math from 'reveal.js/plugin/math/math.esm.js';
+
+
+footer_hider = (event) => {
+
+    var a = event.currentSlide.attributes.getNamedItem('hide-footer');
+
+    if (a) {
+        if (a.nodeValue === 'yes') {
+            document.querySelector('.footer').style.visibility = 'hidden';
+        }
+    } else {
+        document.querySelector('.footer').style.visibility = 'visible';
+    }
+}
+
+
+let deck = new Reveal({
+    plugins: [Markdown, Notes, RevealHighlight, Math],
+    navigationMode: 'linear'
+
+})
+deck.initialize();
+deck.addEventListener('slidechanged', footer_hider);
